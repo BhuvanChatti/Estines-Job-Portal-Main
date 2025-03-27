1d60140b127dcfe2d6d793a0b92a478d02f8adca
BhuvanChatti <bhuvanchatti579@gmail.com>
2025-03-27 18:53:48 +0530

    Update TopMenuBar.js

--- src/pages/TopMenuBar.js
+++ src/pages/TopMenuBar.js
@@ -4,14 +4,24 @@ import './TopMenuBar.css';
 import '../../src/styles/Home.css';
 
 const TopMenuBar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
         <div className="top-menu-bar text-center">
-            <div className="Ham">☰</div>
+            <div className="Ham" onClick={toggleMenu}>☰</div>
             <div className="menu-items">
-                <Link to="/" className="menu-item">Home</Link>
-                <Link to="/jobs" className="menu-item">Jobs</Link>
-                <Link to="/dashboard" className="menu-item">Dashboard</Link>
-                <Link to="/search" className="menu-item">Search</Link>
+                <Link to="/" className="menu-item" onClick={closeMenu}>Home</Link>
+                <Link to="/jobs" className="menu-item" onClick={closeMenu}>Jobs</Link>
+                <Link to="/dashboard" className="menu-item" onClick={closeMenu}>Dashboard</Link>
+                <Link to="/search" className="menu-item" onClick={closeMenu}>Search</Link>
             </div>
             <div className="auth-buttons">
                 <Link to="/login">
