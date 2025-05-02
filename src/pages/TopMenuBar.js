f3827100f1cc07856d12e23a3aa49a05d7563576
BhuvanChatti <bhuvanchatti579@gmail.com>
2025-05-02 10:21:25 +0530

    Update TopMenuBar.js

--- src/pages/TopMenuBar.js
+++ src/pages/TopMenuBar.js
@@ -17,20 +17,21 @@ const TopMenuBar = () => {
     return (
         <div className="top-menu-bar text-center">
             <div className="Ham" onClick={toggleMenu}>☰</div>
-            <div className={`menu-items ${isMenuOpen ? 'active': ''}`}>
+            <div className={`menu-items ${isMenuOpen ? 'active' : ''}`}>
                 <Link to="/" className="menu-item" onClick={closeMenu}>Home</Link>
                 <Link to="/jobs" className="menu-item" onClick={closeMenu}>Jobs</Link>
                 <Link to="/dashboard" className="menu-item" onClick={closeMenu}>Dashboard</Link>
                 <Link to="/search" className="menu-item" onClick={closeMenu}>Search</Link>
             </div>
-            <div className="auth-buttons">
-                <Link to="/login">
-                    <button className="button">Login</button>
-                </Link>
-                <Link to="/register">
-                    <button className="button">Register</button>
-                </Link>
-            </div>
+            {!user && (
+                <div className="auth-buttons">
+                    <Link to="/login">
+                        <button className="button">Login</button>
+                    </Link>
+                    <Link to="/register">
+                        <button className="button">Register</button>
+                    </Link>
+                </div>)}
         </div>
     );
 };
